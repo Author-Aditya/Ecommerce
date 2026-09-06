7e3717d0b7d02b3e46763ed933697f59ea34a19e
adityags913@gmail.com <adityags913@gmail.com>
2026-09-06 14:55:46 +0530

    Commiting the latest changes related to concurrent processesing

--- OrderIntakeService/src/main/java/com/order/intake/controller/OrderController.java
+++ OrderIntakeService/src/main/java/com/order/intake/controller/OrderController.java
@@ -1,16 +1,23 @@
 package com.order.intake.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.order.intake.dto.OrderRequest;
 import com.order.intake.dto.OrderResponse;
 import com.order.intake.service.OrderService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -47,3 +54,4 @@ public class OrderController {
                 .build());
     }
 }
+
--- OrderProcessingService/src/main/java/com/order/processing/service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.order.processing.service.repository;
-
-import com.order.processing.service.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
